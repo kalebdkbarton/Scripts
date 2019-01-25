600d872291bc645dd5c346f0a960a49bc8e69812
Kaleb DK Barton <kbarton@Kalebs-MacBook-Pro.local>
2019-01-24 20:45:45 -0500

    working for my zone

--- zone.py
+++ zone.py
@@ -1,7 +1,6 @@
-#! /usr/bin/python
+#! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 import sys
-file=sys.argv[1]
-if (file == ""):
+if len(sys.argv) <= 1:
     print ("")
     print ("This script will turn a zone file into the correct format for DNS Made Easy.")
     print ("--- Incorrect Syntax ----")
@@ -9,4 +8,55 @@ if (file == ""):
     print ("Example: zone dnsmadeeasy.txt")
     print ("Example: zone constellix.txt")
     print ("")
-    print (file)
+else:
+    zone=sys.argv[1]
+    origin = (str(input("What is the origin domain?\n")))
+    originDot = (origin+".")
+    dotOrigindot = ("."+origin+".")
+    print("Here is your fixed zone:\n\n")
+    if "ORIGIN" not in zone:
+        print ("$ORIGIN "+origin)
+    fh = open(zone)
+    for line in fh:
+        withSemi = ";"+origin
+        if withSemi in line:
+            continue
+        if "ORIGIN" not in line:
+            removeDomain1 = line.replace(dotOrigindot, "")
+            removeDomain2 = removeDomain1.replace(originDot, "@")
+            removeDomain3 = removeDomain2.replace(origin, "@")
+        else:
+            output = line
+        if "SOA" in line:
+            continue
+        elif "A" in line:
+            RECORDTYPE="A"
+        elif "CNAME" in line:
+            RECORDTYPE="CNAME"
+        elif "TXT" in line:
+            RECORDTYPE="TXT"
+        elif "SPF" in line:
+            RECORDTYPE="SPF"
+        elif "MX" in line:
+            RECORDTYPE="MX"
+        elif "CAA" in line:
+            RECORDTYPE="CAA"
+        elif "PTR" in line:
+            RECORDTYPE="PTR"
+        elif "SRV" in line:
+            RECORDTYPE="SRV"
+        elif "NS" in line:
+            RECORDTYPE="NS"
+        else:
+            RECORDTYPE="none"
+        if (RECORDTYPE != "none"):
+            if "IN" not in line:
+                withIn = ("IN\t"+RECORDTYPE)
+                output = removeDomain3.replace(RECORDTYPE, withIn)
+            else:
+                output = removeDomain3
+        else:
+            output = removeDomain3
+        #print(removeDomain2)
+        print(output)
+    fh.close()
